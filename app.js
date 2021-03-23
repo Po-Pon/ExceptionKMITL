08a3f98fc3b739c7c6044c01e6c4121099a0a627
phalat080 <57059698+phalat080@users.noreply.github.com>
2021-03-24 00:38:09 +0700

    build home page

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: './.env'});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', (__dirname,'views'));
+
+app.use(express.static(path.join(__dirname,'./static')));
+
+const dbConnection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+dbConnection.connect((error) =>{
+    if(error) throw err
+    else{
+        console.log("MYSQL Connected...")
+    }
+})
+
+app.get('/', (req,res) => {
+    res.render('home');
+})
+
+app.listen(3000,() => {
+    console.log("Server is running in port 3000....");
+});
