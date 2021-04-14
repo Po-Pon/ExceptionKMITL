f6ed697c0b60c5def52f39c8c354c2e6fff8af02
prathannw <73283786+prathannw@users.noreply.github.com>
2021-04-14 18:41:54 +0700

    frontend login reg จุ้บๆ

---
+++ Dew/frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view :key="$route.fullPath" />
+  </div>
+</template>
+
+<script>
+  // import headerBar from './components/banner_navbar'
+  // import footerBar from './components/footer'
+  export default {
+    name: 'App',
+    // components: {
+    //   headerBar,
+    //   footerBar
+    // }
+  }
+</script>
+
+<style>
+
+</style>
